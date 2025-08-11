68c3cf90064241b0afc882c7543114f364884dd6
hulrog <petar99t@gmail.com>
2025-08-11 18:20:25 +0200

    home page zamenio component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FactsComponent } from './facts/facts.component';
 import { NarrativesComponent } from './narratives/narratives.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'facts', component: FactsComponent },
   { path: 'narratives', component: NarrativesComponent },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
 ];
